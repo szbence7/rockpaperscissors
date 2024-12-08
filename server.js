9c501adac93652adfec1858a203a931b678d4713
BenceIX <benceix@BenceIXs-MacBook-Air.local>
2024-12-08 17:32:00 +0100

    Add name

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
